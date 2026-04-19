ec0831428f8eb8fc059a97cdcbca4f44735794bc
ydevansh140-coder <ydevansh140@gmail.com>
2026-04-19 17:16:46 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: '.',
   plugins: [react()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  },
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
   }
 })
